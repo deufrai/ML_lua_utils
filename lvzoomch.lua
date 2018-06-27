af1a11a6c8293254cf487471d04e9e53fb893eb6
Frédéric CORNU <fcornu@wardsback.org>
2018-06-27 17:47:47 +0200

    reduce target and switch to green

--- lvzoomch.lua
+++ lvzoomch.lua
@@ -1,11 +1,14 @@
+-- print target on zoomed LiveView
+
 function draw_target()
 
+    -- display a 5px radious circle on center of display
     display.circle(
 
         display.width / 2,
         display.height / 2,
-        10,
-        COLOR.WHITE
+        5,
+        COLOR.GREEN1
 
     )
 end
@@ -14,6 +17,10 @@ event.keypress = function(key)
 
     if KEY.ZOOMIN == key and MODE.MOVIE ~= camera.mode and false == menu.visible then
 
+        --[[
+            We want our target to appear when we are zoomed 10x so we check
+            if zoom key is pressed when we currently are zoomed 5x
+        --]]
         if true == lv.enabled and 5 == lv.zoom then
 
             display.draw(draw_target)
