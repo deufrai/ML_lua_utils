749eeedec0607a179760e63d1f156d19e53288d1
Frédéric CORNU <fcornu@wardsback.org>
2018-06-28 16:59:50 +0200

    add GPLv3 preamble

--- lvzoomch.lua
+++ lvzoomch.lua
@@ -1,4 +1,20 @@
--- print target on zoomed LiveView
+-- print target on LiveView when zoomed 10x
+
+-- lvzoomch - Magic Lantern LUA script that prints a "target" on center of LiveView when zoomed 10x
+-- Copyright (C) 2018  Frédéric CORNU
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- ou should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 function draw_target()
 
